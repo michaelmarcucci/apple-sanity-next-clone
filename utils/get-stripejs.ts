bcfd67f3574d714bc1e3e163997702aa62193dc9
michaelmarcucci <64811901+michaelmarcucci@users.noreply.github.com>
2022-09-13 17:56:14 -0400

    build from ILW completed

---
+++ utils/get-stripejs.ts
@@ -0,0 +1,13 @@
+// copied from vercel stripe guide repo @ https://github.com/vercel/next.js/blob/canary/examples/with-stripe-typescript/utils/get-stripejs.ts
+// This is a singleton to ensure we only instantiate Stripe once.
+import { Stripe, loadStripe } from '@stripe/stripe-js';
+
+let stripePromise: Promise<Stripe | null>;
+const getStripe = () => {
+	if (!stripePromise) {
+		stripePromise = loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY!);
+	}
+	return stripePromise;
+};
+
+export default getStripe;
